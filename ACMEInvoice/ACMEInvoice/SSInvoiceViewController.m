2b07785082eddf699e85edabb3c2c24945b44610
Michael Imai <mimai@shaw.ca>
2013-02-08 10:19:01 -0800

    change view controller names

---
+++ ACMEInvoice/ACMEInvoice/SSInvoiceViewController.m
@@ -0,0 +1,29 @@
+//
+//  SSInvoiceViewController.m
+//  ACMEInvoice
+//
+//  Created by Michael Imai on 2013-02-08.
+//  Copyright (c) 2013 Michael Imai. All rights reserved.
+//
+
+#import "SSInvoiceViewController.h"
+
+@interface SSInvoiceViewController ()
+
+@end
+
+@implementation SSInvoiceViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ACMEInvoice/ACMEInvoice/SSProfileViewController.m
@@ -0,0 +1,29 @@
+//
+//  SSProfileViewController.m
+//  ACMEInvoice
+//
+//  Created by Michael Imai on 2013-02-08.
+//  Copyright (c) 2013 Michael Imai. All rights reserved.
+//
+
+#import "SSProfileViewController.h"
+
+@interface SSProfileViewController ()
+
+@end
+
+@implementation SSProfileViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
